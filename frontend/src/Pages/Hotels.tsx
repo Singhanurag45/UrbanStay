b9a4c979c060fba55a89da948cf46d59daced96e
Anurag singh <anuragsinghrbl2002.in>
2026-02-15 01:48:33 +0530

    update hotel page ui

--- frontend/src/Pages/Hotels.tsx
+++ frontend/src/Pages/Hotels.tsx
@@ -287,7 +287,7 @@ const Hotels = () => {
         {/* CONTENT GRID */}
         {loading ? (
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {[1, 2, 3, 4, 5, 6].map((n) => (
+            {[1, 2, 3, 4, 5, 6 , 7, 8 , 9 , 10, 11, 12].map((n) => (
               <HotelSkeleton key={n} />
             ))}
           </div>
